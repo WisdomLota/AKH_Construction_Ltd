d39db188fe4058af273f3a78532309226c02d22b
WisdomLota <ngaloru2019@gmail.com>
2025-03-08 11:55:32 +0300

    Done with the Nav Bar and Hero section, ensured proper adaption to various screen sizes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,54 @@
+import { useState } from "react";
 import AKH_Logo from "../assets/AKHRealtyLogo.png";
+import hamMenu from "../assets/hamMenu.svg";
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
     return (
       <div>
-      <nav className="bg-transparent absolute w-full z-10 px-4 py-6">
-        <div className="container flex space-x-4 justify-end">
-          <div className="justify-center space-x-8 text-white font-medium mt-2">
+      <nav className="bg-transparent absolute w-full z-10 px-4 py-6 mt-2">
+
+        {/*Mobile Menu Button -only visible on mobiles*/}
+        <div className="flex md:hidden justify-between">
+          <button onClick={toggleMenu}>
+            <img src={hamMenu} alt="hamburger menu" />
+          </button>
+          <div className="md:order-2 md:px-16">
+            <img src={AKH_Logo} alt="AKH Realty Logo" className="h-8 md:h-12"/>
+          </div>
+        </div>
+
+        {/* Logo */}
+        
+        <div className="block md:hidden w-8"></div>
+
+        {/*Desktop Menu - hidden on mobile */}
+        <div className="hidden md:flex md:order-1 md:justify-end md:space-x-8 text-white font-medium">
+          <div className="space-x-8 mt-2">
             <a href="#" >HOME</a>
             <a href="#" >EXPLORE</a>
             <a href="#" >ABOUT</a>
             <a href="#" >CONTACT</a>
           </div>
-          <div className="px-16">
-            <img src={AKH_Logo} alt="AKHCON Realty Logo" className="h-12" />
+          <div className="md:order-2 md:px-16">
+            <img src={AKH_Logo} alt="AKH Realty Logo" className="h-16 md:h-12"/>
           </div>
         </div>
+
+        {/*Mobile Menu - slide down when open */}
+        {isMenuOpen && (
+          <div className="md:hidden border-white border-2 w-fit h-fit bg-transparent mt-4 p-4 rounded">
+            <div className="flex flex-col space-y-4 text-white font-medium">
+              <a href="#" >HOME</a>
+              <a href="#" >EXPLORE</a>
+              <a href="#" >ABOUT</a>
+              <a href="#" >CONTACT</a>
+            </div>
+          </div>
+        )}
+
       </nav>
       </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,13 +12,13 @@ const Home = () => {
         {/* Hero Section */}
         <section className="h-screen relative">
 
-          <div className="absolute bottom-0 left-0 right-0 p-16 text-white">
+          <div className="absolute bottom-0 left-0 right-0 p-12 md:p-20 text-white">
             <div className="container">
               <h1 className="text-4xl md:text-6xl font-semibold mb-4">Welcome to<br />AKHCON Realty</h1>
               <p className="mb-8 max-w-md">An estate full of modernized homes with beautiful aesthetics.</p>
 
               <div className="inline-flex items-center pb-2 group cursor-pointer">
-                <span className="mr-4 font-medium border-b-2 border-white ">EXPLORE REALTY</span>
+                <span className="mr-4 font-medium border-b-2 border-white w-36">EXPLORE REALTY</span>
                 <div className="w-8 h-8 rounded-full bg-white flex items-center justify-center">
                   <img src={directArrow} alt="arrow-direction" className="w-4 h-4"/>
                 </div>
